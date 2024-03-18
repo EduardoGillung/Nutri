ce717bcbebc40dd247e9caf91737ab4c9faed601
Eduardo <gillung100@gmail.com>
2024-03-18 20:11:17 -0300

    home funcionando

--- src/Telas/home.js
+++ src/Telas/home.js
@@ -1,30 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { View, TextInput, Button, StyleSheet, Platform, Pressable, Text } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Platform, Pressable, Text, FlatList } from 'react-native';
 import Header from "../Componentes/Header";
 import { ref, get, set, push, onValue, remove, getDatabase, update, child } from 'firebase/database'
 import { db } from "../Serviços/firebase"
 
 
+const TelaHome = ({ navigation }) => {
 
-
-
-
-const TelaHome = ({ navigation}) => {   
+    const [tarefa1, setTarefa1] = useState('')
+    const renderItem = ({ item }) => (
+        <View>
+            <Text>{item.description}</Text>
+        </View>
+    )
+    useEffect(() => {
+        const tarefaRef = ref(db, 'Lista de tarefas/')
     
-useEffect(() => {
-    const tarefaRef = ref(db, 'Lista de tarefas/')
 
     onValue(tarefaRef, (snapshot) => {
         const data = snapshot.val();
-        console.log(data);
+        const arrayData = Object.values(data)
+        console.log(arrayData);
+
+        setTarefa1(arrayData)
+        console.log(tarefa1)
     })
-})
+},[])
                                 
     return (
         <View style={styles.container}>
         <Header/>
         <Pressable onPress={() => navigation.navigate('addTarefa')}>
                 <Text style={styles.register}>Voltar Add tarefas </Text>
+                <FlatList
+                    data={tarefa1}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.taskId}
+                />
+
             </Pressable>            
         </View>
     );
